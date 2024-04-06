924368ae75a3c3c9c5a4fc263a492ddb5fd9b524
korsakov <korsakov.ua.dn@gmail.com>
2024-04-06 18:18:14 +0300

    fix/Import path

--- src/middlewares/authGuard/index.ts
+++ src/middlewares/authGuard/index.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 
-import { HTTP_STATUSES } from "../HTTP_STATUSES";
+import { HTTP_STATUSES } from "../../HTTP_STATUSES";
 
 export const authGuard = (req: Request, res: Response, next: NextFunction) => {
   if (req.query.token) {
