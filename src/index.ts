142c46141ac450178e69398ce04efb11a8b532a0
korsakov <korsakov.ua.dn@gmail.com>
2024-05-05 11:36:45 +0300

    Test module.exports

--- src/index.ts
+++ src/index.ts
@@ -3,20 +3,22 @@ import { runDb } from "./db";
 
 const port = process.env.PORT || 5000;
 
-// export const startApp = async () => {
-//   await runDb();
-
-//   app.listen(port, () => {
-//     console.log(`Example app listening on port ${port}`);
-//   });
-// };
-
-// startApp();
-
-(async () => {
+export const startApp = async () => {
   await runDb();
 
   app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
   });
-})();
+};
+
+startApp();
+
+module.exports = startApp;
+
+// (async () => {
+//   await runDb();
+
+//   app.listen(port, () => {
+//     console.log(`Example app listening on port ${port}`);
+//   });
+// })();
