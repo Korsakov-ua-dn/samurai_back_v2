115887bc979cabeb98c5c5330b115fd52e389c36
korsakov <korsakov.ua.dn@gmail.com>
2024-05-05 11:02:56 +0300

    Added export default startApp

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,10 @@ import express, { Request, Response } from "express";
 import { HTTP_STATUSES } from "./HTTP_STATUSES";
 import { getCoursesRouter } from "./routes/courses";
 import { getTestsRouter } from "./routes/tests";
+<<<<<<< Updated upstream
+=======
+import { db } from "./db/MOCK";
+>>>>>>> Stashed changes
 import { authGuard } from "./middlewares";
 // import { dbMethods } from "./db/MOCK";
 import { dbMethods } from "./db";
--- src/db/index.ts
+++ src/db/index.ts
@@ -1,3 +1,12 @@
+<<<<<<< Updated upstream
 export * from "./methods";
 export { runDb } from "./db";
 export type * from "./types";
+=======
+const mongoUri =
+  process.env.mongoUri || "mongodb://0.0.0.0:27017/?maxPoolSize=20&w=majority";
+
+export const client = new MongoClient();
+
+export async function runDb() {}
+>>>>>>> Stashed changes
