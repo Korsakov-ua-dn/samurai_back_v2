6a4eadb9f6560a8d50e78a114edd72cc923c11c0
korsakov <korsakov.ua.dn@gmail.com>
2024-05-05 11:50:09 +0300

    MOCK db methods fro vercel app, because have not production db

--- src/index.ts
+++ src/index.ts
@@ -3,22 +3,19 @@ import { runDb } from "./db";
 
 const port = process.env.PORT || 5000;
 
-export const startApp = async () => {
-  await runDb();
-
-  app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
-  });
-};
-
-startApp();
-
-module.exports = startApp;
-
-// (async () => {
+// const startApp = async () => {
 //   await runDb();
 
 //   app.listen(port, () => {
 //     console.log(`Example app listening on port ${port}`);
 //   });
-// })();
+// };
+
+// startApp();
+
+/**
+ * For vercel with MOCK dbMethods
+ */
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
